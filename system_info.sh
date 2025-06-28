75859a5ac18220e74c45397278d81cda79da17fb
Yury <55970900+remarkua@users.noreply.github.com>
2025-06-28 03:24:32 +0300

    Delete system_info.sh

--- system_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Информация о системе:"
-echo "--------------------"
-echo "Имя хоста: $(hostname)"
-echo "Версия ядра: $(uname -r)"
-echo "Операционная система:"
-cat /etc/os-release | grep -E 'NAME|VERSION'
-
