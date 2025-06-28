e4bf1420efd18f2275f0a2a2228a5040f0546489
Yury <55970900+remarkua@users.noreply.github.com>
2025-06-28 04:35:34 +0300

    Add files via upload

--- scripts/check_ssh.sh
+++ scripts/check_ssh.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 echo "Проверка состояния разрешения root SSH..."
 if grep -q "PermitRootLogin yes" /etc/ssh/sshd_config; then
- echo "Разрешение root SSH разрешено."
- echo "Хотите запретить? (y/n)"
- read answer
- if "$answer" == "y"; then
- sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
- sudo systemctl restart sshd
- if sudo systemctl is-active --quiet sshd; then
- echo "Служба SSH успешно перезапущена."
- else
- echo "Ошибка при перезапуске службы SSH."
- fi
- echo "Разрешение root SSH было запрещено."
- else
- echo "Изменение разрешения root SSH отменено."
- fi
+    echo "Разрешение root SSH разрешено."
+    echo "Хотите запретить? (y/n)"
+    read answer
+    if [ "$answer" == "y" ]; then
+        sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+        sudo systemctl restart sshd
+        if sudo systemctl is-active --quiet sshd; then
+            echo "Служба SSH успешно перезапущена."
+        else
+            echo "Ошибка при перезапуске службы SSH."
+        fi
+        echo "Разрешение root SSH было запрещено."
+    else
+        echo "Изменение разрешения root SSH отменено."
+    fi
 else
- echo "Разрешение root SSH запрещено."
- echo "Хотите разрешить? (y/n)"
- read answer
- if "$answer" == "y"; then
- sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
- sudo systemctl restart sshd
- if sudo systemctl is-active --quiet sshd; then
- echo "Служба SSH успешно перезапущена."
- else
- echo "Ошибка при перезапуске службы SSH."
- fi
- echo "Разрешение root SSH было разрешено."
- else
- echo "Изменение разрешения root SSH отменено."
- fi
+    echo "Разрешение root SSH запрещено."
+    echo "Хотите разрешить? (y/n)"
+    read answer
+    if [ "$answer" == "y" ]; then
+        sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+        sudo systemctl restart sshd
+        if sudo systemctl is-active --quiet sshd; then
+            echo "Служба SSH успешно перезапущена."
+        else
+            echo "Ошибка при перезапуске службы SSH."
+        fi
+        echo "Разрешение root SSH было разрешено."
+    else
+        echo "Изменение разрешения root SSH отменено."
+    fi
 fi
 
