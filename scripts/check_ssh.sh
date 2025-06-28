056575b5c0bf97331c15817d7776fbffc2599621
Yury <55970900+remarkua@users.noreply.github.com>
2025-06-28 04:11:22 +0300

    Add files via upload

--- scripts/check_ssh.sh
+++ scripts/check_ssh.sh
@@ -4,7 +4,7 @@ if grep -q "PermitRootLogin yes" /etc/ssh/sshd_config; then
  echo "Разрешение root SSH разрешено."
  echo "Хотите запретить? (y/n)"
  read answer
- if "$answer" == "y"; then
+ if [ "$answer" == "y" ]; then
  sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
  systemctl restart sshd
  echo "Разрешение root SSH было запрещено."
@@ -15,7 +15,7 @@ else
  echo "Разрешение root SSH запрещено."
  echo "Хотите разрешить? (y/n)"
  read answer
- if "$answer" == "y"; then
+ if [ "$answer" == "y" ]; then
  sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
  systemctl restart sshd
  echo "Разрешение root SSH было разрешено."
